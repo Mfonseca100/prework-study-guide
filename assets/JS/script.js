4abe9206ddaca0ab42ab029e223436dbfbff607a
Edith <rissafonseca@yahoo.com>
2023-01-23 21:39:21 -0600

    changed updated

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,11 @@
-var topic = "HTML";
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript']; 
+for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
 
+/*
 if (topic === 'HTML') {
-    console.log("Let's study HTML!"); 
+    console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
     console.log("Let's study CSS!");
 } else if (topic === 'Git') {
@@ -11,3 +15,4 @@ if (topic === 'HTML') {
 } else {
     console.log('Please try again!');
 }
+*/
